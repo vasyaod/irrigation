92c52ce4b6efcb5f811b0faf382af2856e47286b
Vasiliy Vazhesov <vasiliy.vazhesov@gmail.com>
2021-02-23 08:38:11 -0800

    new logic for valve controller

--- valve-controller/index.js
+++ valve-controller/index.js
@@ -2,13 +2,19 @@ const mqtt = require('mqtt')
 const { List } = require('immutable');
 const CronJob = require('cron').CronJob;
 
-const client = mqtt.connect("mqtt://192.168.10.105")
-
-client.subscribe("valve/1/status")
-
 let queue = List([])
 
-client.on('message', function (topic, message) {
+function subscribe(topic, f) {
+    const client = mqtt.connect("mqtt://192.168.10.105")
+
+    client.subscribe(topic)
+    
+    client.on('message', function (topic, message) {
+        f(topic, message)
+    })
+}
+
+subscribe("valve/1/status",  function (topic, message) {
     console.log("Valve awake", topic, message.toString())
 
     if (queue.size > 0) {
@@ -18,9 +24,14 @@ client.on('message', function (topic, message) {
         console.log("A new task is available", nextTask.topic, nextTask.value)
         client.publish(nextTask.topic, nextTask.value)
     } else {
-        console.log("No tasks, valve should sleep")
+        console.log("No tasks, the valve should sleep")
         client.publish('valve/1/sleep', '')
     }
+})
+
+subscribe("valve-controller",  function (topic, message) {
+    console.log("Debug method to valve controller", message.toString())
+    queue = queue.push(JSON.parse(message.toString()))
 })
 
 const job = new CronJob('0 0 9 * * *', function() {
